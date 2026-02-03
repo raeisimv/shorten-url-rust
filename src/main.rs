cc7d830e6cc272b3db34df7199b6d8f1a8a8eba7
Morteza Raeisi <mrraeisi@outlook.com>
2026-02-03 21:11:17 +0330

    adjust tracing and logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::info;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // adjust tracing and log
+    let tracing_opt = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into());
+    tracing_subscriber::registry().with(tracing_opt).init();
+
+    info!("done! exiting the app...");
+    Ok(())
 }
