758597bd203cf6f3d5160d013771438a14c7d0c2
Morteza Raeisi <mrraeisi@outlook.com>
2026-02-05 16:19:40 +0330

    organize imports

--- src/shortener.rs
+++ src/shortener.rs
@@ -1,5 +1,9 @@
-use axum::{Json, http::StatusCode};
+use axum::{Json, extract::State, http::StatusCode};
 use serde::{Deserialize, Serialize};
+use sqlx::prelude::*;
+use std::sync::Arc;
+
+use crate::AppState;
 
 #[derive(Deserialize, Debug)]
 pub struct ShortenerRequest {
@@ -13,10 +17,21 @@ pub struct ShortenerResponse {
     pub url: String,
 }
 
+#[derive(FromRow)]
+pub struct UrlModel {
+    pub id: i32,
+    pub short_url: String,
+    pub url: String,
+    pub tag: Option<String>,
+    pub ttl: Option<u32>,
+}
+
 pub async fn shortener_handler(
+    State(state): State<Arc<AppState>>,
     Json(input): Json<ShortenerRequest>,
 ) -> Result<Json<ShortenerResponse>, (StatusCode, String)> {
     let shorten_url = shorten(&input.url);
+
     let response = ShortenerResponse { url: shorten_url };
     Ok(Json(response))
 }
