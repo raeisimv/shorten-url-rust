3ec64e512d37537c1dce36e37843903ae78dbdb8
Morteza Raeisi <mrraeisi@outlook.com>
2026-02-08 20:48:09 +0330

    store the generated shorten url into database

--- src/shortener.rs
+++ src/shortener.rs
@@ -9,7 +9,7 @@ use crate::AppState;
 pub struct ShortenerRequest {
     pub url: String,
     pub tag: Option<String>,
-    pub ttl: Option<u32>,
+    pub ttl: Option<i64>,
 }
 
 #[derive(Serialize, Debug)]
@@ -23,14 +23,14 @@ pub struct UrlModel {
     pub short_url: String,
     pub url: String,
     pub tag: Option<String>,
-    pub ttl: Option<u32>,
+    pub ttl: Option<i64>,
 }
 
 pub async fn shortener_handler(
     State(state): State<Arc<AppState>>,
-    Json(input): Json<ShortenerRequest>,
+    Json(pyl): Json<ShortenerRequest>,
 ) -> Result<Json<ShortenerResponse>, (StatusCode, String)> {
-    let shorten_url = gen_shorten_code(&input.url);
+    let shorten_url = gen_shorten_code(&pyl.url);
 
     // make it as short as possible
     let mut shortest = None;
@@ -51,6 +51,21 @@ pub async fn shortener_handler(
             "Failed to generate short URL".to_string(),
         ));
     };
+
+    // store it
+    sqlx::query!(
+        r#"
+        INSERT INTO Urls (short_url, long_url, tag, ttl) values ($1, $2, $3, $4)
+        "#,
+        shortest,
+        pyl.url,
+        pyl.tag.unwrap_or("default".to_string()),
+        pyl.ttl.unwrap_or_default()
+    )
+    .fetch_optional(&state.db)
+    .await
+    .map_err(internal_error)?;
+
     let response = ShortenerResponse { url: shortest };
     Ok(Json(response))
 }
